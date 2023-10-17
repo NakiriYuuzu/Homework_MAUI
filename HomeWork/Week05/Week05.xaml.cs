b492cc2f6de39bc2e09b318ff05facdec7003a83
yuuzu <yuuzu@yuuzu.net>
2023-10-17 14:54:52 +0800

    update project.

---
+++ HomeWork/Week05/Week05.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork.Week05;
+
+public partial class Week05 : ContentPage
+{
+    public Week05()
+    {
+        InitializeComponent();
+    }
+}
